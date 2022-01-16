7a4987cf9bc5d389766a05d9cdb78739fdd89c9c
Marc Biegota <marc.biegota@5minds.de>
2022-01-16 23:41:49 +0100

    add class ItemWeights

---
+++ Fenrir13/GamePlay/ItemWeights.cs
@@ -0,0 +1,20 @@
+namespace Fenrir13.GamePlay;
+
+public static class ItemWeights
+{
+    public static int DUMBBELL_BAR = 5000;
+    public static int HELMET = 2500;
+    public static int GLOVES = 150;
+    public static int BOOTS = 500;
+    
+    public static Dictionary <string,int> WeightDictionary = new Dictionary<string, int>()
+    {
+        {nameof(DUMBBELL_BAR), DUMBBELL_BAR},
+        {nameof(HELMET), HELMET},
+        {nameof(GLOVES), GLOVES},
+        {nameof(BOOTS), BOOTS}
+    };
+
+
+
+}
